0aec40e63d942c9cd4b7fd8b7fae8a388a545628
Mamadou Mb. Dione <47165947+mbayedione10@users.noreply.github.com>
2025-01-28 11:46:02 +0000

    ✨ feat (about): add

--- src/data_files/data.ts
+++ src/data_files/data.ts
@@ -51,15 +51,15 @@ export const partenaires = [
 
 export const issues = [
     {
-        heading:"Assurer la fiabilité des données sur les initiatives électorales en Afrique",
+        heading:"Ensuring the reliability of data on electoral initiatives in Africa",
         icon: `<svg width="64px" height="64px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#FF5836"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M4 0a4 4 0 00-4 4v8a4 4 0 004 4h8a4 4 0 004-4V4a4 4 0 00-4-4H4zm7.293 3.292a3 3 0 00-4.001 4.001l-4 4a1 1 0 101.415 1.414l4-4a3 3 0 004.001-4.001l-2 2.001a1 1 0 11-1.415-1.414l2-2z" fill="#6EC770"></path> </g></svg>`,
     },
         {
-        heading:"Mettre en lumière les bonnes pratiques",
+        heading:"Highlighting good practice",
         icon: `<svg width="64px" height="64px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#FF5836"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M4 0a4 4 0 00-4 4v8a4 4 0 004 4h8a4 4 0 004-4V4a4 4 0 00-4-4H4zm7.293 3.292a3 3 0 00-4.001 4.001l-4 4a1 1 0 101.415 1.414l4-4a3 3 0 004.001-4.001l-2 2.001a1 1 0 11-1.415-1.414l2-2z" fill="#6EC770"></path> </g></svg>`,
     },
         {
-        heading:"Promouvoir la participation citoyenne",
+        heading:"Promoting citizen participation",
         icon: `<svg width="64px" height="64px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#FF5836"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M4 0a4 4 0 00-4 4v8a4 4 0 004 4h8a4 4 0 004-4V4a4 4 0 00-4-4H4zm7.293 3.292a3 3 0 00-4.001 4.001l-4 4a1 1 0 101.415 1.414l4-4a3 3 0 004.001-4.001l-2 2.001a1 1 0 11-1.415-1.414l2-2z" fill="#6EC770"></path> </g></svg>`,
     },
 ]
