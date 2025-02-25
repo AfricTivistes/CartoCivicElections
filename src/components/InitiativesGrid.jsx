d05c6b3962269fea46c4faeefffc11a40676276f
Mamadou Mb. Dione <47165947+mbayedione10@users.noreply.github.com>
2025-02-25 16:32:55 +0000

    Restored to '69954d6ed30b89ae701557dcd343dec20da85d30'
    Replit-Restored-To:69954d6ed30b89ae701557dcd343dec20da85d30

--- src/components/InitiativesGrid.jsx
+++ src/components/InitiativesGrid.jsx
@@ -4,7 +4,6 @@ import Card from './Card';
 const InitiativesGrid = ({ initiatives, language }) => {
   const [selectedCategory, setSelectedCategory] = useState('');
   const [selectedCountry, setSelectedCountry] = useState('');
-  const apiEndpoint = language === 'fr' ? '/fr/api/map_fr.json' : '/api/map.json';
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
