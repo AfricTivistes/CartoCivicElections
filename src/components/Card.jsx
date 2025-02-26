d21e96bf9e41cbcc1a53ad4b0ec82a2f89d7e1df
Mamadou Mb. Dione <47165947+mbayedione10@users.noreply.github.com>
2025-02-26 15:28:37 +0000

    Assistant checkpoint: Rendre les liens de filtrage cliquables dans Card.jsx
    Assistant generated file changes:
    - src/components/Card.jsx: Rendre pays et thématique cliquables
    
    ---
    
    User prompt:
    
    dans la page detage initiatives,
    rendre cliquable payes et thematique vers le filtre specifique

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,7 +15,7 @@ const Card = ({ title, country, category, description, langue, logo = "" }) => {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-primary-500 mr-2" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
           </svg>
-          <a href={`${basePath}?tags=${encodeURIComponent(country)}`} className="text-lg text-primary-700 hover:underline">
+          <a href={`${basePath}?country=${encodeURIComponent(country)}`} className="text-lg text-primary-700 hover:underline">
             {country}
           </a>
         </div>
@@ -25,7 +25,7 @@ const Card = ({ title, country, category, description, langue, logo = "" }) => {
             <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
             <path fillRule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clipRule="evenodd" />
           </svg>
-          <a href={`${basePath}?tags=${encodeURIComponent(category)}`} className="text-primary-700 hover:underline">
+          <a href={`${basePath}?category=${encodeURIComponent(category)}`} className="text-primary-700 hover:underline">
             {category}
           </a>
         </div>
@@ -34,7 +34,7 @@ const Card = ({ title, country, category, description, langue, logo = "" }) => {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-primary-500 mr-2" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clipRule="evenodd" />
           </svg>
-          <a href={`${basePath}?tags=${encodeURIComponent(description)}`} className="text-gray-600 hover:text-primary-600">
+          <a href={`${basePath}?thematic=${encodeURIComponent(description)}`} className="text-gray-600 hover:text-primary-600">
             {description}
           </a>
         </div>
